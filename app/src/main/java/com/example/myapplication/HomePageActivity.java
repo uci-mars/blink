1b0ade3adce851cc08cf3b15c67b98a9a6b1d70f
Erikka Linn <eeelinn@gmail.com>
2018-10-13 18:34:52 -0700

    Added profile photo

--- app/src/main/java/com/example/myapplication/HomePageActivity.java
+++ app/src/main/java/com/example/myapplication/HomePageActivity.java
@@ -3,15 +3,19 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button gotoUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
+        findViewById(R.id.gotoUpdate).setOnClickListener(this);
 //        // Get the Intent that started this activity and extract the string
 //        Intent intent = getIntent();
 //        String[] user_and_pass = intent.getStringArrayExtra(MainActivity.EXTRA_MESSAGE);
@@ -21,5 +25,13 @@ public class HomePageActivity extends AppCompatActivity {
 //        TextView PasstextView = findViewById(R.id.pass_display);
 //        UsertextView.setText(user_and_pass[0]);
 //        PasstextView.setText(user_and_pass[1]);
+    }
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.gotoUpdate:
+                startActivity(new Intent(this, UpdateProfile.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/NewAccountCreation.java
+++ app/src/main/java/com/example/myapplication/NewAccountCreation.java
@@ -1,12 +1,14 @@
 package com.example.myapplication;
 
-import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,48 +16,29 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class NewAccountCreation extends AppCompatActivity implements View.OnClickListener {
 
-    EditText editTextEmailID, editTextPassID, editTextName, editTextPhone;
+    EditText editTextEmailID, editTextPassID;
     private FirebaseAuth mAuth;
-    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account_creation);
-
         editTextEmailID = (EditText) findViewById(R.id.emailID);
         editTextPassID = (EditText) findViewById(R.id.passID);
-        editTextName = (EditText) findViewById(R.id.nameText);
-        editTextPhone = (EditText) findViewById(R.id.phoneText);
 
-
-        databaseReference = FirebaseDatabase.getInstance().getReference("user");
         mAuth = FirebaseAuth.getInstance();
         findViewById(R.id.signUpButton).setOnClickListener(this);
-        System.out.println("FINISHED SETTING UP");
     }
 
     private void registerUser() {
-        final String email = editTextEmailID.getText().toString().trim();
-        final String password = editTextPassID.getText().toString().trim();
-        final String name = editTextName.getText().toString().trim();
-        final String phone = editTextPhone.getText().toString().trim();
+        String email = editTextEmailID.getText().toString().trim();
+        String password = editTextPassID.getText().toString().trim();
 
-
-        if(name.isEmpty()) {
-            editTextEmailID.setError("Name is required");
-            editTextEmailID.requestFocus();
-        }
-
-        if(phone.isEmpty()) {
-//            phone = "";
-            editTextEmailID.setError("Phone is required");
+        if(email.isEmpty()) {
+            editTextEmailID.setError("Email is required");
             editTextEmailID.requestFocus();
         }
 
@@ -69,22 +52,11 @@ public class NewAccountCreation extends AppCompatActivity implements View.OnClic
             editTextEmailID.requestFocus();
         }
 
-
-
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()) {
                     Toast.makeText(getApplicationContext(), "User Registered!", Toast.LENGTH_SHORT).show();
-
-                    String id = databaseReference.push().getKey();
-                    UserInformation userInfo = new UserInformation(name, id, phone);
-
-                    mAuth.signInWithEmailAndPassword(email, password);
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    databaseReference.child(user.getUid()).setValue(userInfo);
-
-                    startActivity(new Intent(NewAccountCreation.this, HomePageActivity.class));
                 }
                 else {
                     if(task.getException() instanceof FirebaseAuthUserCollisionException)
@@ -94,8 +66,6 @@ public class NewAccountCreation extends AppCompatActivity implements View.OnClic
                 }
             }
         });
-
-
     }
 
     @Override
@@ -106,5 +76,6 @@ public class NewAccountCreation extends AppCompatActivity implements View.OnClic
                 break;
         }
     }
+
 
 }
