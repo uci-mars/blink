69ffa9bfce8b0b2ca9676c2af332f4bd635cf57f
Erikka Linn <eeelinn@gmail.com>
2018-10-13 21:30:16 -0700

    Add Profile Picture

--- app/src/main/java/com/example/myapplication/HomePageActivity.java
+++ app/src/main/java/com/example/myapplication/HomePageActivity.java
@@ -3,14 +3,18 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button gotoUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        findViewById(R.id.gotoUpdate).setOnClickListener(this);
 
 //        // Get the Intent that started this activity and extract the string
 //        Intent intent = getIntent();
@@ -21,5 +25,14 @@ public class HomePageActivity extends AppCompatActivity {
 //        TextView PasstextView = findViewById(R.id.pass_display);
 //        UsertextView.setText(user_and_pass[0]);
 //        PasstextView.setText(user_and_pass[1]);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.gotoUpdate:
+                startActivity(new Intent(this, UpdateProfile.class));
+                break;
+        }
     }
 }
